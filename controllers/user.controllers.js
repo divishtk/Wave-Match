af27ea8a78ce266ec125ba9d7f181d6a1e6b0341
divishtk <divisht.avion@hotmail.com>
2025-05-19 17:27:16 +0530

    Refactor user controller and update routes to include user feed and delete functionalities

--- controllers/user.controllers.js
+++ controllers/user.controllers.js
@@ -1,82 +1,126 @@
 import { User } from "../models/users.model.js";
 
 export const userSignUpCOntroller = async (req, resp) => {
-    try {
-        const { firstName, lastName, email, password } = req.body;
+  try {
+    const { firstName, lastName, email, password } = req.body;
 
-        const user = await User.findOne({ email });
+    const user = await User.findOne({ email });
 
-        if (user) {
-            return resp.status(400).json({
-                success: false,
-                message: "Account already exixts",
-            });
-        }
-
-        const userDB = await User.create({
-            firstName,
-            lastName,
-            email,
-            password,
-        });
-
-        await userDB.save();
-        const createdUserCheck = await User.findById(userDB._id);
-        if (!createdUserCheck) {
-            return resp.status(400).json({
-                success: false,
-                message: "Something went wrong while user creation",
-            });
-        }
-
-        return resp.status(400).json({
-            data: createdUserCheck,
-            success: true,
-            message: "Register Success",
-        });
-    } catch (error) {
-        return resp.status(400).json({
-            success: false,
-            message: error.message,
-        });
+    if (user) {
+      return resp.status(400).json({
+        success: false,
+        message: "Account already exixts",
+      });
     }
+
+    const userDB = await User.create({
+      firstName,
+      lastName,
+      email,
+      password,
+    });
+
+    await userDB.save();
+    const createdUserCheck = await User.findById(userDB._id);
+    if (!createdUserCheck) {
+      return resp.status(400).json({
+        success: false,
+        message: "Something went wrong while user creation",
+      });
+    }
+
+    return resp.status(400).json({
+      data: createdUserCheck,
+      success: true,
+      message: "Register Success",
+    });
+  } catch (error) {
+    return resp.status(400).json({
+      success: false,
+      message: error.message,
+    });
+  }
 };
 
+export const getUserByEmail = async (req, resp) => {
+  const email = req.body.email;
 
-
-export const getUserByEmail = async(req,resp) =>{
-    const email = req.body.email ;
-
-   try {
-   const user = await User.find({
-            email
-     })
-
-     if(user.length === 0 ) {
-        return resp.status(404).json({
-            success: false,
-            message: "User not found",
-        });
-     }
-
-     return resp.status(200).json({
-        data: user,
-        success: true,
-        message: "Fetch Success",
+  try {
+    const user = await User.find({
+      email,
     });
 
-     
-
-   } catch (error) {
-    console.log(error)
-    return resp.status(400).json({
+    if (user.length === 0) {
+      return resp.status(404).json({
         success: false,
-        message: "Something went wrong while fetching",
+        message: "User not found",
+      });
+    }
+
+    return resp.status(200).json({
+      data: user,
+      success: true,
+      message: "Fetch Success",
     });
-   }
-}
+  } catch (error) {
+    console.log(error);
+    return resp.status(400).json({
+      success: false,
+      message: "Something went wrong while fetching",
+    });
+  }
+};
 
+export const getFeedOfUsers = async (req, resp) => {
+  try {
+    const allUsers = await User.find({});
 
-export const getFeedOfUsers = async(req,resp) =>{
+    if (allUsers.length === 0) {
+      return resp.status(404).json({
+        success: false,
+        message: "No registered users as of now..",
+      });
+    }
 
-}
+    return resp.status(200).json({
+      data: allUsers,
+      success: true,
+      message: "Fetch Success",
+    });
+  } catch (error) {
+    console.log(error);
+    return resp.status(400).json({
+      success: false,
+      message: "Something went wrong while fetching all users",
+    });
+  }
+};
+
+export const deleteUser = async (req, resp) => {
+  const user = req.body.userId;
+
+  try {
+    const deleteUser = await User.findByIdAndDelete({
+      //user
+      _id: user,
+    });
+
+    if (!deleteUser) {
+      return resp.status(401).json({
+        success: false,
+        message: "User is already deleted or it doesnot exists",
+      });
+    }
+
+    return resp.status(400).json({
+      success: false,
+      message: "User deleted successfully",
+    });
+  } catch (error) {
+    console.log(error);
+    return resp.status(400).json({
+      success: false,
+      message: "Error while deleting user",
+    });
+  }
+};
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
-import { getUserByEmail, userSignUpCOntroller } from "../controllers/user.controllers.js";
+import { deleteUser, getFeedOfUsers, getUserByEmail, userSignUpCOntroller } from "../controllers/user.controllers.js";
 
 const userRouter = Router();
 
 
 userRouter.route("/sign-up").post(userSignUpCOntroller)
 userRouter.route("/get-user-from-email").get(getUserByEmail)
-
+userRouter.route("/get-user-feed").get(getFeedOfUsers)
+userRouter.route("/delete-user").delete(deleteUser)
 
 
 export default userRouter;
