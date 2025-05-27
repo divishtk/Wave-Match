7dc6a6b440069c18ced19f422757ed84d3ac116c
divishtk <divisht.avion@hotmail.com>
2025-05-27 20:31:09 +0530

    Implement yourConnections endpoint to fetch accepted connections

--- controllers/connection.controllers.js
+++ controllers/connection.controllers.js
@@ -8,7 +8,10 @@ export const getConnectionRequests = async (req, resp) => {
       toUserId: loggedInUser._id,
       status: "intrested",
       // }).populate("fromUserId",["firstName","lastName"]);
-    }).populate("fromUserId", "firstName lastName age about hobbies gender pic");
+    }).populate(
+      "fromUserId",
+      "firstName lastName age about hobbies gender pic"
+    );
 
     return resp.status(201).json({
       success: true,
@@ -17,6 +20,44 @@ export const getConnectionRequests = async (req, resp) => {
     });
   } catch (error) {
     return resp.status(201).json({
+      success: true,
+      message: error.message,
+    });
+  }
+};
+
+export const yourConnections = async (req, resp) => {
+  const loggedInUser = req.user;
+
+  try {
+    // const yourConnections = await ConnectionRequest.find({
+    //   toUserId: loggedInUser,
+    //   status:"accepted"
+    // }).populate("fromUserId",["firstName"]);
+
+    const yourConnections = await ConnectionRequest.find({
+        $or :[
+            {
+                toUserId:loggedInUser._id,
+                status:"accepted" 
+            },
+            {
+                fromUserId:loggedInUser._id,
+                status:"accepted"
+            } 
+        ]
+    }).populate("fromUserId","firstName lastName age about hobbies gender pic")
+  
+     const data = yourConnections.map((data) => data.fromUserId) ;
+
+    return resp.status(201).json({
+      success: true,
+      message: "Your connections",
+      data
+    });
+  } catch (error) {
+    console.log(error);
+    return resp.status(201).json({        
       success: true,
       message: error.message,
     });
--- routes/connections.routes.js
+++ routes/connections.routes.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import authenticationMiddleware from "../middlewares/auth.middlewares.js";
-import { getConnectionRequests } from "../controllers/connection.controllers.js";
+import { getConnectionRequests, yourConnections } from "../controllers/connection.controllers.js";
 
 const connectionRouter = Router();
 
 //endpoint for pending connection request
 connectionRouter.route("/user/requests/received").get(authenticationMiddleware,getConnectionRequests)
 
+connectionRouter.route("/user/connections").get(authenticationMiddleware,yourConnections)
 
 
 
